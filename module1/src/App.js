6e2c69e54bb5d1c7eb190ad2541e6e4973d28aac
ivdanchuk <ivdanchuk@gmail.com>
2024-02-23 19:44:15 +0200

    implement search form ok

--- module1/src/App.js
+++ module1/src/App.js
@@ -1,12 +1,18 @@
 import { Component } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
+import SearchForm from "./components/SearchForm";
 
 class App extends Component {
+  handleSearch = (query) => {
+    console.log("Searching:", query);
+  };
+
   render() {
     return (
       <div className="App">
-        <Counter initValue={10} />
+        {/* <Counter></Counter> */}
+        <SearchForm initialQuery="" onSearch={this.handleSearch} />
       </div>
     );
   }
--- module1/src/components/SearchForm.js
+++ module1/src/components/SearchForm.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 
-function SearchForm() {
-  return <div>SearchForm</div>;
+class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: this.props.initialQuery || "Search movie ...",
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ query: event.target.value });
+  };
+
+  handleSearch = () => {
+    this.props.onSearch && this.props.onSearch(this.state.query);
+  };
+
+  handleKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      this.handleSearch();
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.query}
+          onChange={this.handleInputChange}
+          onKeyDown={this.handleKeyDown}
+        />
+        <button onClick={this.handleSearch}>Search</button>
+      </div>
+    );
+  }
 }
 
 export default SearchForm;
