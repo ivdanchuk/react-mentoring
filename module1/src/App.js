9d8b95b488c26a3cc54cb0a0562fa3aef339d2dc
ivdanchuk <ivdanchuk@gmail.com>
2024-02-27 16:19:59 +0200

    fix & refactoring GenreSelect

--- module1/src/App.js
+++ module1/src/App.js
@@ -1,33 +1,37 @@
 import { Component } from "react";
-import "./css/App.css";
+import "./css/App.scss";
 import SearchForm from "./components/SearchForm";
 import GenreSelect from "./components/GenreSelect";
 import Counter from "./components/Counter";
 
 class App extends Component {
+  state = {
+    genres: ["ALL", "Crime", "Comedy", "Drama", "Horror"],
+    selectedGenre: "Comedy",
+  };
+
   handleSearch = (query) => {
     console.log("Searching:", query);
   };
 
   handleSelect = (genre) => {
-    console.log("Selected genre:", genre);
+    this.setState({ selectedGenre: genre });
   };
 
   render() {
-    const genres = ["ALL", "Crime", "Comedy", "Drama", "Horror"];
-    const selectedGenre = "Comedy";
-
     return (
       <div>
         <div className="header">
           <SearchForm initialQuery="" onSearch={this.handleSearch} />
         </div>
         <GenreSelect
-          genres={genres}
-          selectedGenre={selectedGenre}
+          genres={this.state.genres}
+          selectedGenre={this.state.selectedGenre}
           onSelect={this.handleSelect}
         />
-        <Counter initValue={10}></Counter>
+        <div>
+          <Counter initValue={10}></Counter>
+        </div>
       </div>
     );
   }
--- module1/src/components/GenreSelect.js
+++ module1/src/components/GenreSelect.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 class GenreSelect extends React.Component {
   render() {
     const { genres, selectedGenre, onSelect } = this.props;
     return (
-      <div className="genre-select">
+      // <div className="genre-select">
+      //   <ul style={{ display: "flex", listStyleType: "none" }}>
+      //     {genres.map((genre) => (
+      //       <li
+      //         key={genre}
+      //         className={`genre-select-button ${
+      //           genre === selectedGenre ? "genre-select-button--selected" : ""
+      //         }`}
+      //         onClick={() => onSelect(genre)}
+      //       >
+      //         {genre}
+      //       </li>
+      //     ))}
+      //   </ul>
+      // </div>
+      <div className="movie-genre">
         {genres.map((genre) => (
-          <button
+          <div
+            className={`genre-item ${selectedGenre === genre ? "active" : ""}`}
             key={genre}
-            className={`genre-select-button ${
-              genre === selectedGenre ? "genre-select-button--selected" : ""
-            }`}
+            //className={this.props.selectedGenre === genre ? "selected" : ""}
             onClick={() => onSelect(genre)}
           >
             {genre}
-          </button>
+          </div>
         ))}
       </div>
     );
